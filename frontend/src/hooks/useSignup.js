4f6115e70388b1a6f7de6a9ca78706b9cc752411
Sangita-M123 <153701193+Sangita-M123@users.noreply.github.com>
2024-04-02 17:12:58 +0530

    Update useSignup.js

--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -44,7 +44,7 @@ const useSignup = () => {
       localStorage.setItem("chat-user", JSON.stringify(data));
       setAuthUser(data);
     } catch (error) {
-      toast.error(error.message);
+      toast.error(error.message,error);
     } finally {
       setLoading(false);
     }

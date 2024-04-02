d1749d5ae16ec813a3d734e9f272bf1517b34e32
Sangita-M123 <153701193+Sangita-M123@users.noreply.github.com>
2024-04-02 10:16:52 +0530

    Update useGetConversations.js

--- frontend/src/hooks/useGetConversations.js
+++ frontend/src/hooks/useGetConversations.js
@@ -9,7 +9,7 @@ const useGetConversations = () => {
       setLoading(true);
       try {
         const res = await fetch(
-          "https://2484d1d7-965f-4234-9c16-685119e29ddb-00-3vnfkc3kz4k62.sisko.replit.dev/api/users",
+          "api/users",
         );
         const data = await res.json();
         if (data.error) {
